e77b0d973cb9e00d6851fec7a05ec847bc8d9682
Ronald Albert <ronaldalbert1609@gmail.com>
2017-04-22 01:33:07 -0300

    GravityMovement

---
+++ LudumDareProject/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float gravityForce;
+    [SerializeField]
+    float speed;
+    float dirX;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        dirX = Input.GetAxis("Horizontal");
+
+        rb.velocity = transform.right * dirX * speed * Time.deltaTime;
+
+        rb.AddForce(-transform.up * gravityForce, ForceMode2D.Force);
+    }
+}
